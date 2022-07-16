61ab21875ec40be7bb3900258c7c03fd1c60477c
SafteyDav3 <talktomedw@gmail.com>
2022-07-15 19:54:31 -0500

    Working

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates,
+        quisquam.
+      </p>
+    </div>
+  );
+}
