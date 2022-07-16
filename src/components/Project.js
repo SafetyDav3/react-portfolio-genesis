aebf45ca93cd20e29e7ed5a94fe2f17caf1d52d9
SafteyDav3 <talktomedw@gmail.com>
2022-07-15 19:15:58 -0500

    Project.js

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,7 +3,19 @@ import React from "react";
 function Project() {
   return (
     <div>
-      <h1>This is the Project page!</h1>
+      /* Display a card for project. */
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">Project Title</h5>
+          <p className="card-text">
+            lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Voluptates, quisquam.
+          </p>
+          <a href="#" className="btn btn-primary">
+            View Project
+          </a>
+       </div>
+     </div>
     </div>
   );
 }
