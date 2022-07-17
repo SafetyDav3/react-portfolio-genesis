bcb9569bcb01bb0f5bcfc1be5d81a6b002545f49
SafteyDav3 <talktomedw@gmail.com>
2022-07-17 13:43:40 -0500

    nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,7 +4,6 @@ import Portfolio from "./Portfolio";
 import Contact from "./Contact";
 import Resume from "./Resume";
 
-
 import { Tabs } from "@mantine/core";
 
 import {
@@ -15,25 +14,31 @@ import {
 } from "react-icons/bs";
 
 function Nav() {
-    return (
-      <Tabs color="cyan" tabSize="lg"  position="right" tabPadding="xl" variant="outline">
-        <Tabs.Tab label="About" icon={<BsInfoSquareFill size={14} />}>
-          <About />
-        </Tabs.Tab>
-        <Tabs.Tab label="Portfolio" icon={<BsFillFolderFill size={14} />}>
-          <Portfolio />
-        </Tabs.Tab>
-        <Tabs.Tab label="Contact" icon={<BsFillChatRightTextFill size={14} />}>
-          <Contact />
-        </Tabs.Tab>
-        <Tabs.Tab
-          label="Resume"
-          icon={<BsReverseLayoutTextSidebarReverse size={14} />}
-        >
-          <Resume />
-        </Tabs.Tab>
-      </Tabs>
-    );
+  return (
+    <Tabs
+      color="cyan"
+      tabSize="lg"
+      position="right"
+      tabPadding="xl"
+      variant="outline"
+    >
+      <Tabs.Tab label="About" icon={<BsInfoSquareFill size={14} />}>
+        <About />
+      </Tabs.Tab>
+      <Tabs.Tab label="Portfolio" icon={<BsFillFolderFill size={14} />}>
+        <Portfolio />
+      </Tabs.Tab>
+      <Tabs.Tab label="Contact" icon={<BsFillChatRightTextFill size={14} />}>
+        <Contact />
+      </Tabs.Tab>
+      <Tabs.Tab
+        label="Resume"
+        icon={<BsReverseLayoutTextSidebarReverse size={14} />}
+      >
+        <Resume />
+      </Tabs.Tab>
+    </Tabs>
+  );
 }
 
 export default Nav;
