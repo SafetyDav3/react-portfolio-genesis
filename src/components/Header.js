38e9aef2dacdeb9e45a1ad282b335b164d0a80a4
SafteyDav3 <talktomedw@gmail.com>
2022-07-16 20:03:12 -0500

    Icons for nav and mantine

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,13 @@
 import React from "react";
 import Nav from "./Nav";
 
-
 function Header() {
   return (
-    <div>
-      <h1>SafteyDav3</h1>
-      <Nav />
-    </div>
-  );
+      <div>
+        <h1>SafteyDav3</h1>
+        <Nav />
+      </div>
+    );
 }
 
 export default Header;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,46 @@
 import React from "react";
 
+import { Tabs } from "@mantine/core";
+
+import { Photo, MessageCircle, Settings } from "tabler-icons-react";
+
+import {
+  BsFillFolderFill,
+  BsInfoSquareFill,
+  BsFillChatRightTextFill,
+  BsReverseLayoutTextSidebarReverse,
+} from "react-icons/bs";
+
 function Nav() {
-  return (
-    <div>
-      <a href="/About">About Me</a>
-      <a href="/Portfolio">Portfolio</a>
-      <a href="/Contact">Contact</a>
-      <a href="/Resume">Resume</a>
-    </div>
-  );
+    return (
+      <Tabs>
+        <Tabs.Tab label="About" icon={<BsInfoSquareFill size={14} />}>
+          Gallery tab content
+        </Tabs.Tab>
+        <Tabs.Tab label="Portfolio" icon={<BsFillFolderFill size={14} />}>
+          Settings tab content
+        </Tabs.Tab>
+        <Tabs.Tab label="Contact" icon={<BsFillChatRightTextFill size={14} />}>
+          Messages tab content
+        </Tabs.Tab>
+        <Tabs.Tab
+          label="Resume"
+          icon={<BsReverseLayoutTextSidebarReverse size={14} />}
+        >
+          Settings tab content
+        </Tabs.Tab>
+      </Tabs>
+    );
+  
+  
+  // return (
+  //   <div>
+  //       <a href="/About">About Me</a>
+  //       <a href="/Portfolio">Portfolio</a>
+  //       <a href="/Contact">Contact</a>
+  //       <a href="/Resume">Resume</a>
+  //   </div>
+  // );
 }
 
 export default Nav;
